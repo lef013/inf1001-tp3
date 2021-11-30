0f11e2d78403ca8d09459803a72554fd313a1ea8
MLefebvre <mathieu.lefebvre@cogeco.com>
2021-11-30 15:06:06 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { useState, useEffect, useRef } from 'react';
-// Import @tensorflow/tfjs-core
-import * as tf from '@tensorflow/tfjs-core';
 // Adds the WebGL backend to the global backend registry.
 import '@tensorflow/tfjs-backend-webgl';
 // https://github.com/tensorflow/tfjs-models#readme
 import * as mobilenet from '@tensorflow-models/mobilenet';
 // Bootstrap
-import Accordion from 'react-bootstrap/Accordion';
+import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
@@ -22,14 +18,30 @@ import Spinner from './components/Spinner/Spinner';
 import styles from './App.module.css';
 
 function App() {
+  /*******************************************************************************
+   * STATE
+   ******************************************************************************/
   const [isModalLoading, setisModalLoading] = useState(false); // boolean
   const [model, setModel] = useState(null); // object
   const [imageURL, setImageURL] = useState(null); // string
   const [results, setResults] = useState([]); // object
+  const [showModelError, setShowModelError] = useState(false); // boolean
 
+  /*******************************************************************************
+   * UI / REF
+   ******************************************************************************/
   const imageRef = useRef();
-  // const fileInputRef = useRef();
   const textInputUrlRef = useRef();
+
+  /*******************************************************************************
+   * ON MOUNT
+   ******************************************************************************/
+  // Passing an empty object to deps will make it load only once
+  // @param effect — Imperative function that can return a cleanup function
+  // @param deps — If present, effect will only activate if the values in the list change.
+  useEffect(() => {
+    loadModel();
+  }, []);
 
   const loadModel = async () => {
     setisModalLoading(true);
@@ -37,50 +49,61 @@ function App() {
       // Load the mobilenet Model
       const model = await mobilenet.load();
       setModel(model);
-      console.log('loadModel() - const model ', model);
+      // console.log('loadModel() - const model ', model);
       setisModalLoading(false);
     } catch (error) {
-      console.log(error);
+      // console.log(error);
       setisModalLoading(false);
+      setShowModelError(true);
     }
   };
+  /*******************************************************************************
+   * ON UPDATE
+   ******************************************************************************/
 
-  // const fileChangeHandler = (event) => {
-  //   // console.log(event);
-  //   const { files } = event.target;
-  //   if (files.length > 0) {
-  //     const url = URL.createObjectURL(files[0]);
-  //     console.log('fileChangeHandler() - const url ', url);
-  //     setImageURL(url);
-  //   } else {
-  //     setImageURL(null);
-  //   }
-  // };
+  /*******************************************************************************
+   * HANDLERS
+   ******************************************************************************/
+  const clickAlertHandler = () => {
+    window.location.reload();
+  };
 
   const urlChangeHandler = (event) => {
     // console.log(event);
     setImageURL(event.target.value);
-    console.log('urlChangeHandler() - value ', event.target.value);
+    // console.log('urlChangeHandler() - value ', event.target.value);
+    setResults([]);
   };
 
-  const identify = async () => {
+  const identifyHandler = async () => {
     // `classify` takes an input image element and returns an array with top classes and their probabilities.
     // `current` points to the mounted element
     textInputUrlRef.current.value = null;
     const results = await model.classify(imageRef.current);
-    console.log('identify() - const results ', results);
+    // console.log('identify() - const results ', results);
     setResults(results);
   };
 
-  // @param effect — Imperative function that can return a cleanup function
-  // @param deps — If present, effect will only activate if the values in the list change.
-  // Passing an empty object to deps will make it load only once
-  useEffect(() => {
-    loadModel();
-  }, []);
-
+  /*******************************************************************************
+   * RETURN
+   ******************************************************************************/
   if (isModalLoading) {
     return <Spinner />;
+  }
+
+  if (showModelError) {
+    return (
+      <Container>
+        <Alert className={styles.alert} variant="danger" onClose={() => clickAlertHandler()} dismissible>
+          <Alert.Heading>Erreur</Alert.Heading>
+          <p>
+            Ouvrir la console pour voir l'erreur.
+            <br />
+            En fermant cette alerte, vous allez rafraîchir la page.
+          </p>
+        </Alert>
+      </Container>
+    );
   }
 
   return (
@@ -91,22 +114,21 @@ function App() {
           <h1 className={styles.title}>Identification d'images</h1>
         </Row>
         {/* File Input */}
-        <Row>
-          {/* <Col md={6}>
-            <Form.Group controlId="formFile" className="mb-3">
-              <Form.Label>Choisir le fichier</Form.Label>
-              <Form.Control type="file" accept="image/*" onChange={fileChangeHandler} />
-            </Form.Group>
-          </Col> */}
+        <Row className={styles.row_centerred}>
           <Col>
             <Form.Group controlId="formUrl" className="mb-3">
               <Form.Label>Copier un lien URL</Form.Label>
               <Form.Control type="text" placeholder="URL..." ref={textInputUrlRef} onChange={urlChangeHandler} />
             </Form.Group>
           </Col>
+          <Col>
+            <Button className={styles.button_photo} variant="info" href="https://picsum.photos/images" target="_blank">
+              Picsum Photos
+            </Button>
+          </Col>
         </Row>
         <Row>
-          <Col md={9} className={styles.image__col}>
+          <Col md={9} className={styles.image_col}>
             {/* If imageURL is not null will show <Image> */}
             {imageURL && (
               <Image
@@ -120,7 +142,7 @@ function App() {
             )}
           </Col>
           <Col md={3}>
-            {/* If imageURL is not null will show <Image> */}
+            {/* If results is not null will show <ListGroup> */}
             {results.length > 0 && (
               <React.Fragment>
                 {results.map((element, index) => {
@@ -139,10 +161,15 @@ function App() {
             )}
           </Col>
         </Row>
+        {/* If imageURL is not null will show <Button> */}
         {imageURL && (
-          <Button className="float-end" variant="success" size="lg" onClick={identify}>
-            Identifier l'image
-          </Button>
+          <Row>
+            <Col>
+              <Button className={styles.button_identify} variant="success" size="lg" onClick={identifyHandler}>
+                Identifier l'image
+              </Button>
+            </Col>
+          </Row>
         )}
       </Container>
     </React.Fragment>
